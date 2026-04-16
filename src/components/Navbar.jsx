1f2b9afb31651f2f71c5f3897f58794aa0dc9d83
Kallol Dey <kalloldey067@gmail.com>
2026-04-16 18:30:05 +0600

    responsive navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,66 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { FaChartLine, FaHome } from "react-icons/fa";
-import { IoMdTime } from 'react-icons/io';
+import { IoMdMenu, IoMdTime } from 'react-icons/io';
 
 const Navbar = () => {
     const pathname = usePathname();
+
     return (
-        <div className="navbar flex bg-base-100 shadow-sm p-5">
-            <div className="flex-1 pl-10">
-                { <Image src={navImg} alt="logo" width={0} height={0} />}
+        <div className="navbar bg-base-100 shadow-sm px-4 md:px-10 py-4">
+
+            <div className="navbar-start">
+
+                <div className="dropdown md:hidden">
+                    <label tabIndex={0} className="btn btn-ghost text-2xl">
+                        <IoMdMenu />
+                    </label>
+
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                        <li className={`${pathname === '/' ? 'bg-green-900 text-white rounded-lg' : ''}`}>
+                            <Link className="flex gap-2 items-center p-2" href="/">
+                                <FaHome /> Home </Link>
+                        </li>
+
+                        <li className={`${pathname === '/timeline' ? 'bg-green-900 text-white rounded-lg' : ''}`}>
+                            <Link className="flex gap-2 items-center p-2" href="/timeline">
+                                <IoMdTime /> Timeline </Link>
+                        </li>
+
+                        <li className={`${pathname === '/starts' ? 'bg-green-900 text-white rounded-lg' : ''}`}>
+                            <Link className="flex gap-2 items-center p-2" href="/starts">
+                                <FaChartLine /> Starts </Link>
+                        </li>
+                    </ul>
+                </div>
+
+                
+                <Image src={navImg} alt="logo" className="w-24 md:w-32 h-auto ml-2" />
             </div>
-            <div className="flex-none">
-                <ul className="flex gap-5 pr-10">
-                    <li className={`${pathname === '/' ? 'bg-green-900 text-white rounded-lg' : ''}`}><Link className='flex gap-2 justify-center items-center p-2' href="/"><FaHome /> Home</Link></li>
 
-                    <li className={`flex gap-2 justify-center items-center ${pathname === '/timeline' ? 'bg-green-900 text-white rounded-lg' : ''}`}><Link className='flex gap-2 justify-center items-center p-2' href="/timeline"><IoMdTime /> Timeline</Link></li>
+            
+            <div className="navbar-end hidden md:flex">
+                <ul className="menu menu-horizontal px-1 gap-2">
+                    <li className={`${pathname === '/' ? 'bg-green-900 text-white rounded-lg' : ''}`}>
+                        <Link className='flex gap-2 items-center p-2' href="/">
+                            <FaHome /> Home
+                        </Link>
+                    </li>
 
-                    <li className={`flex gap-2 justify-center items-center ${pathname === '/starts' ? 'bg-green-900 text-white rounded-lg' : ''}`}><Link className='flex gap-2 justify-center items-center p-2' href="/starts"><FaChartLine /> Starts</Link></li>    
+                    <li className={`${pathname === '/timeline' ? 'bg-green-900 text-white rounded-lg' : ''}`}>
+                        <Link className='flex gap-2 items-center p-2' href="/timeline">
+                            <IoMdTime /> Timeline
+                        </Link>
+                    </li>
+
+                    <li className={`${pathname === '/starts' ? 'bg-green-900 text-white rounded-lg' : ''}`}>
+                        <Link className='flex gap-2 items-center p-2' href="/starts">
+                            <FaChartLine /> Starts
+                        </Link>
+                    </li>    
                 </ul>
             </div>
         </div>
@@ -27,4 +71,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
